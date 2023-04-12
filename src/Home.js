6f44e72cc2d24f46e434d02ae784954aed90d1d0
MoharTry <103957742+MoharTry@users.noreply.github.com>
2023-04-12 23:18:56 +0530

    UI LoginPage

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="navbar">
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
