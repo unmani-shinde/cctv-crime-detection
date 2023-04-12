10eb8c40db76d35f6f293835541bd60582462df6
MoharTry <103957742+MoharTry@users.noreply.github.com>
2023-04-12 23:16:10 +0530

    UI homepage

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import "./home.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="d-flex text-center text-white">
+        <div
+          id="Home"
+          className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column"
+        >
+          <div id="navbar" className="navbar">
+            <nav>
+              <div id="grp">
+                <ul>
+                  <li>
+                    <a href="#">Name: Mohar</a>
+                  </li>
+                  <li>
+                    <a href="#">Staff ID: 123</a>
+                  </li>
+                  <li>
+                    <a href="#">Crime Branch: Mumbai</a>
+                  </li>
+                  <li>
+                    <a href="#">Location: Mulund</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+          <div id="px-3">
+            <h1>Weapon Detection</h1>
+            <p className="lead">
+              {" "}
+              Welcome to our weapon detection system. Our advanced technology
+              uses a combination of AI algorithms and image processing to detect
+              weapons in real-time. Our system is designed to accurately
+              identify various types of weapons, including firearms, knives, and
+              explosives, with high accuracy and speed. With our system in
+              place, you can rest assured that your premises are safe and
+              secure. In the event that a weapon is detected, our system will
+              immediately notify the appropriate authorities, allowing them to
+              take swift action and prevent potential harm.
+            </p>
+            <a href="/" className="btn border-white">
+              View Live Feed
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
